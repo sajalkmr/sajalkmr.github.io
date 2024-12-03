67e128748ef1c7c3d52c6ad2e50d7eeeee08b05b
Sajal <sajal.kumar2003@gmail.com>
2024-12-04 01:50:05 +0530

    Setup GitHub Actions deployment

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '',
   images: {
     unoptimized: true,
   },
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/' : '',
+  trailingSlash: true,
 };
 
 export default nextConfig;
